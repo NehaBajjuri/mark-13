ab0940017b44f4adf14a20dc661a59f4197ee79e
NehaBajjuri <105218064+NehaBajjuri@users.noreply.github.com>
2022-11-01 07:45:15 +0530

    checkingIsThatAPalindrome

--- app.js
+++ app.js
@@ -3,21 +3,71 @@ function reverseStr(str){
     var listOfChars = str.split('');
     var reverseListOfChars = listOfChars.reverse();
     var reversedStr = reverseListOfChars.join('');
-    //return str.split('').reverse().join('')--in asingle line
+    //return str.split('').reverse().join('')--in a single line
 
     return reversedStr;
 }
 function isPalindrome(str)
 {
-var reverse = reverseStr(str)
-if(str === reverse)
-{
-    return true;
-}
-else{
-    return false;
-}
+var reverse = reverseStr(str);
+return str===reverse;
 
 }
-console.log(isPalindrome('madam'));
-console.log(isPalindrome('vikatakavi'));
+
+
+function convertDateToStr(date)
+{
+   
+    var dateStr={day:'',month:'',year:''};
+    if(date.day<10)
+    {
+        dateStr.day ='0'+date.day;
+    }
+    else{
+        dateStr.day=date.day.toString();
+    }
+    if(date.month<10)
+    {
+        dateStr.month ='0'+date.month;
+    }
+    else{
+        dateStr.month=date.month.toString();
+    }
+    dateStr.year = date.year.toString();
+    return dateStr;
+}
+
+
+function getAllDateFormats(date)
+{
+  var dateStr = convertDateToStr(date);
+  var ddmmyyyy = dateStr.day + dateStr.month + dateStr.year;
+  var mmddyyyy = dateStr.month + dateStr.day + dateStr.year;
+  var yyyymmdd = dateStr.year + dateStr.month + dateStr.day;
+  var mmddyy =dateStr.month + dateStr.day + dateStr.year.slice(-2);
+  var yymmdd =dateStr.year.slice(-2) + dateStr.month + dateStr.day;
+  var ddmmyy= dateStr.day+dateStr.month+dateStr.year.slice(-2);
+return [ddmmyyyy, mmddyyyy, yyyymmdd,mmddyy, yymmdd, ddmmyy];
+
+}
+var date={
+    day: 20,
+    month:2,
+    year:2002
+};
+
+function checkPalin(date)
+{
+    var listOfPalin=getAllDateFormats(date);
+    var flag = false;
+    for(var i=0;i<listOfPalin.length;i++)
+    {
+        if(isPalindrome(listOfPalin[i]))
+        {
+           flag=true;
+            break;
+        }
+    }
+    return flag;
+}
+console.log(checkPalin(date));
